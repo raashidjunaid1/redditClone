19b09929c18ebbb9b6b24c379b99d933f99bcebb
raashidjunaid1 <raashidjunaid1@gmail.com>
2020-08-23 19:28:03 +0100

    removed dist

--- server/dist/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IS_PROD = void 0;
-exports.IS_PROD = process.env.NODE_ENV === 'production';
-//# sourceMappingURL=constants.js.map
